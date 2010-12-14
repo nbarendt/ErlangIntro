192837a89ca70e26e59bf7368518226b5d800cac
Nick Barendt <nick@barendt.com>
2010-12-14 20:56:00 +0000

    quiet sp.erl

--- sp.erl
+++ sp.erl
@@ -10,19 +10,19 @@ test(NumberOfProcesses) ->
     end.
 
 f(0, ParentPid) ->
-    io:format("Last Process Pid: ~p~n", [self()]),
+    %io:format("Last Process Pid: ~p~n", [self()]),
     % send a message (just our Pid) to our parent
-    io:format("~p Sending message to ~p~n", [self(), ParentPid]),
+    %io:format("~p Sending message to ~p~n", [self(), ParentPid]),
     ParentPid ! self();
 f(N, ParentPid) ->
     % recursively spawn children
     ChildPid = spawn(?MODULE, f, [N-1, self()]),
-    io:format("~p Waiting for ~p~n", [self(), ChildPid]),
+    %io:format("~p Waiting for ~p~n", [self(), ChildPid]),
     % wait for a message from our child
     receive
         ChildPid ->
             % send a message to our parent
-            io:format("~p Sending message to ~p~n", [self(), ParentPid]),
+            %io:format("~p Sending message to ~p~n", [self(), ParentPid]),
             ParentPid ! self()
     end.
 
